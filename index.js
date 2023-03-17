b418bd09cd8833b48a96e4c67d4f59cb9a0609f4
Codebox124 <rajcodes733@gmail.com>
2023-03-17 14:18:35 -0700

    update ui

---
+++ index.js
@@ -0,0 +1,8 @@
+const HideDiv = document.getElementById('hide-div')
+const HideText = document.getElementById('hide-text')
+
+console.log('ekwhndefub');
+
+HideText.addEventListener("click", () => {
+    HideDiv.classList.toggle("hidden");
+  });
